ccb5a5c234d1bd6c6d8714d7417695aa949bcc00
Amit Saran <amitsaran@gmail.com>
2020-06-02 16:56:34 -0400

    changes for heroku deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.json({"data": "Hello World!!!"})
+    res.json({"data": "Hello World - deployed from travis ci!!!"})
 });
 
 app.listen(PORT, () => {
