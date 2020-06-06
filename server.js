69d9aa82988c71853c3a7c54b768d23d24507326
Amit Saran <amitsaran@gmail.com>
2020-06-06 03:16:20 -0400

    minor changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.json({"data": "Hello World - docker-todo-node - should deploy"})
+    res.json({"data": "Hello World - docker-todo-node - should deploy - demo"})
 });
 
 app.listen(PORT, () => {
